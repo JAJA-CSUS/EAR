eae212ae2d1fe47fe49b7d5ef685a7c44d1f4bc3
aleo101 <55769947+aleo101@users.noreply.github.com>
2019-12-02 15:28:00 -0800

    create uicubbyPic.py

---
+++ gui/uiCubbyPic.py
@@ -0,0 +1,201 @@
+#Programmer: Alexander Leones
+#Project: Elderly Assistance Robot
+#  uiCubbyPic.py 
+# first semester lab prototype ui
+# Nov 3, 2019
+import time
+import tkinter as tk
+from tkinter.ttk import *
+import RPi.GPIO as GPIO
+
+#cubby gpio IDs
+cubby1=21
+cubby2=20
+cubby3=16
+cubby4=12
+cubby1Input=26
+
+space1=False #space 1 is initially empty
+space2=False
+space3=False
+space4=False
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(cubby1, GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(cubby2, GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(cubby3, GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(cubby4, GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(cubby1Input, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+master = tk.Tk()
+master.title("Assistant Robot GUI V2")
+master.geometry("600x100")
+
+ONlabel1 = Label(master, text="Cubby Empty (system start)")
+ONlabel1.grid(row=0, column=0)
+ONlabel2 = Label(master, text="Cubby Empty (system start)")
+ONlabel2.grid(row=0, column=1)
+ONlabel3 = Label(master, text="Cubby Empty (system start)")
+ONlabel3.grid(row=0, column=2)
+ONlabel4 = Label(master, text="Cubby Empty (system start)")
+ONlabel4.grid(row=0, column=3)
+
+def cubby1Button():
+	global space1
+	if(space1==False):
+		GPIO.output(cubby1, GPIO.HIGH) #signal storage output.
+		while not GPIO.input(cubby1Input):
+			Cubby1StoreButton.config(state='disabled') 
+			Cubby2StoreButton.config(state='disabled') 
+			Cubby3StoreButton.config(state='disabled') 
+			Cubby4StoreButton.config(state='disabled')
+			master.update()
+			time.sleep(0.1)
+		Cubby1StoreButton.config(state='normal')
+		Cubby2StoreButton.config(state='normal')
+		Cubby3StoreButton.config(state='normal')
+		Cubby4StoreButton.config(state='normal') 
+		master.update()
+		space1=True           #space1 is occupied? True
+		ONlabel1.config(text = "Cubby ocupied") #Display it's full
+	else:
+		GPIO.output(cubby1,GPIO.LOW)
+		while not GPIO.input(cubby1Input):
+			Cubby1StoreButton.config(state='disabled') 
+			Cubby2StoreButton.config(state='disabled') 
+			Cubby3StoreButton.config(state='disabled') 
+			Cubby4StoreButton.config(state='disabled') 
+			master.update()
+			time.sleep(0.1)
+		Cubby1StoreButton.config(state='normal')
+		Cubby2StoreButton.config(state='normal')
+		Cubby3StoreButton.config(state='normal')
+		Cubby4StoreButton.config(state='normal') 
+		master.update()
+		space1=False
+		ONlabel1.config(text="Cubby Empty")	
+
+def cubby2Button():
+	global space2
+	if(space2==False):
+		GPIO.output(cubby2, GPIO.HIGH) #signal storage op.
+		while not GPIO.input(cubby1Input):
+			Cubby1StoreButton.config(state='disabled') 
+			Cubby2StoreButton.config(state='disabled') 
+			Cubby3StoreButton.config(state='disabled') 
+			Cubby4StoreButton.config(state='disabled') 
+			master.update()
+			time.sleep(0.1)
+		Cubby1StoreButton.config(state='normal')
+		Cubby2StoreButton.config(state='normal')
+		Cubby3StoreButton.config(state='normal')
+		Cubby4StoreButton.config(state='normal') 
+		master.update()
+		space2=True           #space1 is occupied? True
+		ONlabel2.config(text = "Cubby ocupied") #Display it's full
+	else:
+		GPIO.output(cubby2,GPIO.LOW)
+		while not GPIO.input(cubby1Input):
+			Cubby1StoreButton.config(state='disabled') 
+			Cubby2StoreButton.config(state='disabled') 
+			Cubby3StoreButton.config(state='disabled') 
+			Cubby4StoreButton.config(state='disabled') 
+			master.update()
+			time.sleep(0.1)
+		Cubby1StoreButton.config(state='normal')
+		Cubby2StoreButton.config(state='normal')
+		Cubby3StoreButton.config(state='normal')
+		Cubby4StoreButton.config(state='normal') 
+		master.update()
+		space2=False
+		ONlabel2.config(text="Cubby Empty")
+
+def cubby3Button():
+	global space3
+	if(space3==False):
+		GPIO.output(cubby3, GPIO.HIGH)
+		while not GPIO.input(cubby1Input):
+			Cubby1StoreButton.config(state='disabled') 
+			Cubby2StoreButton.config(state='disabled') 
+			Cubby3StoreButton.config(state='disabled') 
+			Cubby4StoreButton.config(state='disabled') 
+			master.update()
+			time.sleep(0.1)
+		Cubby1StoreButton.config(state='normal')
+		Cubby2StoreButton.config(state='normal')
+		Cubby3StoreButton.config(state='normal')
+		Cubby4StoreButton.config(state='normal') 
+		master.update()
+		space3=True
+		ONlabel3.config(text = "Cubby ocupied") #Display it's full
+	else:
+		GPIO.output(cubby3, GPIO.LOW)
+		while not GPIO.input(cubby1Input):
+			Cubby1StoreButton.config(state='disabled') 
+			Cubby2StoreButton.config(state='disabled') 
+			Cubby3StoreButton.config(state='disabled') 
+			Cubby4StoreButton.config(state='disabled') 
+			master.update()
+			time.sleep(0.1)
+		Cubby1StoreButton.config(state='normal')
+		Cubby2StoreButton.config(state='normal')
+		Cubby3StoreButton.config(state='normal')
+		Cubby4StoreButton.config(state='normal') 
+		master.update()
+		space3=False
+		ONlabel3.config(text="Cubby Empty")
+def cubby4Button():
+	global space4
+	if(space4==False):
+		GPIO.output(cubby4, GPIO.HIGH) #signal storage op.
+		while not GPIO.input(cubby1Input):
+			Cubby1StoreButton.config(state='disabled') 
+			Cubby2StoreButton.config(state='disabled') 
+			Cubby3StoreButton.config(state='disabled') 
+			Cubby4StoreButton.config(state='disabled') 
+			master.update()
+			time.sleep(0.1)
+		Cubby1StoreButton.config(state='normal')
+		Cubby2StoreButton.config(state='normal')
+		Cubby3StoreButton.config(state='normal')
+		Cubby4StoreButton.config(state='normal') 
+		master.update()
+		space4=True           #space1 is occupied? True
+		ONlabel4.config(text = "Cubby ocupied") #Display it's full
+	else:
+		GPIO.output(cubby4,GPIO.LOW)
+		while not GPIO.input(cubby1Input):
+			Cubby1StoreButton.config(state='disabled') 
+			Cubby2StoreButton.config(state='disabled') 
+			Cubby3StoreButton.config(state='disabled') 
+			Cubby4StoreButton.config(state='disabled') 
+			master.update()
+			time.sleep(0.1)
+		Cubby1StoreButton.config(state='normal')
+		Cubby2StoreButton.config(state='normal')
+		Cubby3StoreButton.config(state='normal')
+		Cubby4StoreButton.config(state='normal') 
+		master.update()
+		space4=False
+		ONlabel4.config(text="Cubby Empty")
+
+
+
+def ExitButton():
+	Exitbutton = tk.Button(master, text="Exit", command= EandD, bg='red')
+	Exitbutton.grid(row=1, column=8, ipadx=10, ipady=10)
+def EandD():
+	master.destroy()
+	GPIO.cleanup()
+
+self.photo = PhotoImage(file = r"./greetimg2.png")
+Cubby1StoreButton = tk.Button(master, text="GPIO 21 Cubby 1", image = self.photo, command=cubby1Button)
+Cubby1StoreButton.grid(row=1, column=0, ipadx=20, ipady=20)
+Cubby2StoreButton = Button(master, text="GPIO 20 Cubby 2", command=cubby2Button)
+Cubby2StoreButton.grid(row=1, column=1, ipadx=20, ipady=20)
+Cubby3StoreButton = Button(master, text="GPIO 16 Cubby 3", command=cubby3Button)
+Cubby3StoreButton.grid(row=1, column=2, ipadx=20, ipady=20)
+Cubby4StoreButton = Button(master, text="GPIO XX Cubby 3", command=cubby4Button)
+Cubby4StoreButton.grid(row=1, column=3, ipadx=20, ipady=20)
+ExitButton()
+
+master.mainloop()
