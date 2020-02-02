8a9967d1440901914a58f04a4ac2dd0ad77aab95
aleo101 <aleones101@gmail.com>
2020-02-02 13:26:24 -0800

    working but ugly fix to led bool logic

--- gui/uiCubbyCondensed.py
+++ gui/uiCubbyCondensed.py
@@ -39,8 +39,19 @@ Onlabel3.grid(row=0, column=2)
 Onlabel4 = Label(master, text="Cubby Empty (system start)")
 Onlabel4.grid(row=0, column=3)
 
-def cubbyButton(space, cubbyID, label):
-	
+def cubbyButton(spaceNum, cubbyID, label):
+	if spaceNum==1:
+		global space1 
+		space = space1
+	elif spaceNum==2:
+		global space2
+		space = space2
+	elif spaceNum==3:
+		global space3
+		space = space3
+	elif spaceNum==4:
+		global space4
+		space = space4
 	if(space==False):
 		GPIO.output(cubbyID, GPIO.HIGH) #signal storage op.
 		while not GPIO.input(cubby1Input):
@@ -55,10 +66,11 @@ def cubbyButton(space, cubbyID, label):
 		Cubby3StoreButton.config(state='normal')
 		Cubby4StoreButton.config(state='normal') 
 		master.update()
-		space=True           #space1 is occupied? True
+		#space=True           #space1 is occupied? True
+		spaceTF(spaceNum)
 		label.config(text = "Cubby ocupied") #Display it's full
 	else:
-		GPIO.output(cuubyID,GPIO.LOW)
+		GPIO.output(cubbyID,GPIO.LOW)
 		while not GPIO.input(cubby1Input):
 			Cubby1StoreButton.config(state='disabled') 
 			Cubby2StoreButton.config(state='disabled') 
@@ -71,25 +83,37 @@ def cubbyButton(space, cubbyID, label):
 		Cubby3StoreButton.config(state='normal')
 		Cubby4StoreButton.config(state='normal') 
 		master.update()
-		space=False
+		#space=False
+		spaceTF(spaceNum)
 		label.config(text="Cubby Empty")
 
-
-
+		
 def ExitButton():
 	Exitbutton = tk.Button(master, text="Exit", command= EandD, bg='red')
 	Exitbutton.grid(row=1, column=8, ipadx=10, ipady=10)
 def EandD():
 	master.destroy()
 	GPIO.cleanup()
-
-Cubby1StoreButton = Button(master, text="GPIO 21 Cubby 1", command= partial(cubbyButton, space1, cubby1, Onlabel1))
+def spaceTF(spaceNum):
+	if spaceNum==1:
+		global space1 
+		space1= not space1
+	elif spaceNum==2:
+		global space2 
+		space2= not space2
+	elif spaceNum==3:
+		global space3 
+		space3= not space3
+	elif spaceNum==4:
+		global space4 
+		space4= not space4
+Cubby1StoreButton = Button(master, text="GPIO 21 Cubby 1", command= partial(cubbyButton, 1, cubby1, Onlabel1))
 Cubby1StoreButton.grid(row=1, column=0, ipadx=20, ipady=20)
-Cubby2StoreButton = Button(master, text="GPIO 20 Cubby 2", command= partial(cubbyButton, space2, cubby2, Onlabel2))
+Cubby2StoreButton = Button(master, text="GPIO 20 Cubby 2", command= partial(cubbyButton, 2, cubby2, Onlabel2))
 Cubby2StoreButton.grid(row=1, column=1, ipadx=20, ipady=20)
-Cubby3StoreButton = Button(master, text="GPIO 20 Cubby 3", command= partial(cubbyButton, space3, cubby3, Onlabel3))
+Cubby3StoreButton = Button(master, text="GPIO 20 Cubby 3", command= partial(cubbyButton, 3, cubby3, Onlabel3))
 Cubby3StoreButton.grid(row=1, column=2, ipadx=20, ipady=20)
-Cubby4StoreButton = Button(master, text="GPIO 20 Cubby 4", command= partial(cubbyButton, space4, cubby4, Onlabel4))
+Cubby4StoreButton = Button(master, text="GPIO 20 Cubby 4", command= partial(cubbyButton, 4, cubby4, Onlabel4))
 Cubby4StoreButton.grid(row=1, column=3, ipadx=20, ipady=20)
 ExitButton()
 
