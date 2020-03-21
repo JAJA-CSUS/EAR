a8c357e2e919684ea42977a0a5a6583797d08a28
aleo101 <aleones101@gmail.com>
2020-03-20 20:37:20 -0700

    fixed loc pics stored in

--- gui/scripts/webcam.sh
+++ gui/scripts/webcam.sh
@@ -2,4 +2,4 @@
 
 DATE=$(date +"%Y-%m-%d_%H%M")
 
-fswebcam --no-banner /home/pi/Documents/PiFun/UI/EAR/gui/greetimg1.jpg
+fswebcam --no-banner /home/pi/Documents/PiFun/UI/EAR/gui/images/greetimg1.jpg
